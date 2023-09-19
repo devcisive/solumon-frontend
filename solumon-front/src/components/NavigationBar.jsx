5f1851a1e30f481726d48ed4cf6d50901ccaafc5
chaeeunj <official2930@naver.com>
2023-09-20 01:25:46 +0900

    Feat: (chaeeun/feature/component/#3) 네비게이션 바 컴포넌트 구현

--- solumon-front/src/components/NavigationBar.jsx
+++ solumon-front/src/components/NavigationBar.jsx
@@ -1,28 +1,66 @@
 import styled, { ThemeProvider } from 'styled-components';
 import theme from '../style/theme';
+import { FaUserCircle } from 'react-icons/fa';
+import { useState } from 'react';
 
 function NavigationBar() {
+  const [userInfo, setUserInfo] = useState(null);
+
   return (
     <ThemeProvider theme={theme}>
-      <StyledDiv>
-        <Title>Example</Title>
-      </StyledDiv>
+      <Wrapper>
+        <Logo>솔루몬</Logo>
+        {userInfo && (
+          <User>
+            <UserName>
+              <StyledSpan>{userInfo.name}</StyledSpan>님
+            </UserName>
+            <ProfileIcon />
+          </User>
+        )}
+      </Wrapper>
     </ThemeProvider>
   );
 }
 
 export default NavigationBar;
 
-const StyledDiv = styled.div`
-  margin-top: 100px;
-  width: 300px;
-  height: 300px;
-  background-color: ${({ theme }) => theme.linen};
+const Wrapper = styled.div`
+  position: relative;
+  width: 100vw;
+  height: 55px;
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  background-color: ${({ theme }) => theme.medium_purple};
 `;
 
-const Title = styled.h1`
-  text-align: center;
-  font-size: 18px;
-  font-weight: 600;
-  color: ${({ theme }) => theme.medium_purple};
+const Logo = styled.p`
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%, 0);
+  font-weight: bold;
+  color: ${({ theme }) => theme.linen};
+`;
+
+const User = styled.p`
+  position: absolute;
+  right: 30px;
+  color: ${({ theme }) => theme.linen};
+  display: flex;
+  align-items: center;
+  gap: 20px;
+`;
+
+const UserName = styled.p``;
+
+const StyledSpan = styled.span`
+  font-weight: 500;
+  margin-right: 3px;
+`;
+
+const ProfileIcon = styled(FaUserCircle)`
+  width: 24px;
+  height: 24px;
+  color: ${({ theme }) => theme.linen};
 `;
