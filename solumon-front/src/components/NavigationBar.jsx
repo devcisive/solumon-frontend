a3c0edb4fd89662a3138441b47eec6bc34363ffc
chaeeunj <official2930@naver.com>
2023-09-21 01:39:41 +0900

    Update: (chaeeun/feature/component/#3) 네비게이션 바와 유저 메뉴 컴포넌트 합침

--- solumon-front/src/components/NavigationBar.jsx
+++ solumon-front/src/components/NavigationBar.jsx
@@ -1,10 +1,20 @@
 import styled, { ThemeProvider } from 'styled-components';
 import theme from '../style/theme';
 import { FaUserCircle } from 'react-icons/fa';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function NavigationBar() {
   const [userInfo, setUserInfo] = useState(null);
+  const [userMenuActive, setUserMenuActive] = useState(false);
+
+  const handleProfileIconClick = () => {
+    setUserMenuActive(!userMenuActive);
+    console.log(userMenuActive);
+  };
+
+  // useEffect(() => {
+  //   console.log(userMenuActive);
+  // }, [userMenuActive]);
 
   return (
     <ThemeProvider theme={theme}>
@@ -13,10 +23,21 @@ function NavigationBar() {
         {userInfo && (
           <User>
             <UserName>
-              <StyledSpan>{userInfo.name}</StyledSpan>님
+              <StyledSpan>userInfo.name</StyledSpan>님
             </UserName>
-            <ProfileIcon />
+            <ProfileIcon onClick={handleProfileIconClick} />
           </User>
+        )}
+        {userMenuActive && (
+          <UserMenuWrapper>
+            <MenuWrapper>
+              <Menu>알림</Menu>
+              <Menu>회원정보</Menu>
+              <Menu>내가 남긴 기록</Menu>
+              <Menu>로그아웃</Menu>
+              <Menu>회원탈퇴</Menu>
+            </MenuWrapper>
+          </UserMenuWrapper>
         )}
       </Wrapper>
     </ThemeProvider>
@@ -41,6 +62,7 @@ const Logo = styled.p`
   transform: translate(-50%, 0);
   font-weight: bold;
   color: ${({ theme }) => theme.linen};
+  cursor: pointer;
 `;
 
 const User = styled.p`
@@ -63,4 +85,42 @@ const ProfileIcon = styled(FaUserCircle)`
   width: 24px;
   height: 24px;
   color: ${({ theme }) => theme.linen};
+  cursor: pointer;
+`;
+
+const UserMenuWrapper = styled.div`
+  position: absolute;
+  top: -35px;
+  right: -58px;
+  display: flex;
+  width: 130px;
+  height: 170px;
+  border-radius: 10px 0 10px 10px;
+  background-color: ${({ theme }) => theme.linen};
+  margin: 100px;
+  padding: 0 5px;
+
+  &::before {
+    content: '';
+    border-left: 20px solid;
+    /* border-right: 12px solid; */
+    border-bottom: 20px solid;
+    border-color: transparent transparent ${({ theme }) => theme.linen}
+      transparent;
+    position: absolute;
+    bottom: 170px;
+    right: 0;
+  }
+`;
+
+const MenuWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 12px;
+`;
+
+const Menu = styled.div`
+  padding-left: 12px;
+  color: ${({ theme }) => theme.dark_purple};
 `;
