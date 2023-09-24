ea44d31e6de5a3dd1efd597a899603ba9d22bc14
eunsoo-cho <127170596+eunsoo-cho@users.noreply.github.com>
2023-09-24 23:32:42 +0900

    :wrench: [Feature/component/#3] 탭바 구현
    Feat:(eunsoo-cho/feature/component/#3) 탭바 구현

---
+++ solumon-front/src/components/TabsComponent.jsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
+import PropTypes from 'prop-types';
+
+function TabsComponent({ tabLabels, defaultTab = 0 }) {
+  const [value, setValue] = React.useState(defaultTab);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  TabsComponent.propTypes = {
+    tabLabels: PropTypes.arrayOf(PropTypes.string).isRequired,
+    defaultTab: PropTypes.number.isRequired,
+  };
+  return (
+    <Box sx={{ width: '40%' }}>
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        aria-label="basic tabs example"
+      >
+        {tabLabels.map((label, index) => (
+          <Tab key={index} label={label} />
+        ))}
+      </Tabs>
+    </Box>
+  );
+}
+export default TabsComponent;
