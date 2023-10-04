404e841428cd7f0d73a27fe46e1e696405c5ddc1
chaeeunj <official2930@naver.com>
2023-10-05 03:15:39 +0900

    :white_check_mark:Update: (chaeeun/feature/component/#15) 게시판 리스트 페이지 라우팅

--- solumon-front/src/App.jsx
+++ solumon-front/src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavigationBar from './components/NavigationBar';
 import SignUpGeneral from './pages/sign-up/SignUpGeneral';
 import SignUpKakao from './pages/sign-up/SignUpKakao';
+import PostList from './pages/PostList';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route path="/sign-up/general" element={<SignUpGeneral />} />
           <Route path="/sign-up/kakao" element={<SignUpKakao />} />
+          <Route path="/post-list" element={<PostList />} />
         </Routes>
       </Router>
     </>
