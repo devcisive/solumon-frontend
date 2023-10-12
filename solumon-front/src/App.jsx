4858cd4e4d3e4bff5e1e8db74b0eb1743fd81846
chaeeunj <official2930@naver.com>
2023-10-13 06:17:46 +0900

    ✅Update: (chaeeun/feature/component/#15) 작업중인 페이지들 라우팅 & 리코일 사용을 위해 전체 태그들 RecoilRoot로 감싸줌

--- solumon-front/src/App.jsx
+++ solumon-front/src/App.jsx
@@ -1,24 +1,38 @@
 import { Reset } from 'styled-reset';
+import { RecoilRoot } from 'recoil';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import NavigationBar from './components/NavigationBar';
+import UserInfo from './pages/UserInfo';
+import ChoiceInterest from './pages/ChoiceInterest';
+import Login from './pages/Login';
 import SignUpGeneral from './pages/sign-up/SignUpGeneral';
 import SignUpKakao from './pages/sign-up/SignUpKakao';
+import FindPassword from './pages/FindPassword';
+import WithDraw from './pages/WithDraw';
 import PostList from './pages/PostList';
+import PostCategory from './pages/PostCategory';
 
 function App() {
   return (
-    <>
+    <RecoilRoot>
       <Reset />
       <Router>
         <NavigationBar />
         <Routes>
-          <Route path="/sign-up/general" element={<SignUpGeneral />} />
-          <Route path="/sign-up/kakao" element={<SignUpKakao />} />
-          <Route path="/post-list" element={<PostList />} />
+          <Route path="/user" element={<UserInfo />} />
+          <Route path="/user/interests" element={<ChoiceInterest />} />
+          <Route path="/user/sign-in/general" element={<Login />} />
+          <Route path="/user/sign-up/general" element={<SignUpGeneral />} />
+          <Route path="/user/sign-up/kakao" element={<SignUpKakao />} />
+          <Route path="/user/find-password" element={<FindPassword />} />
+          <Route path="/user/withdraw" element={<WithDraw />} />
+          <Route path="/posts/post-list" element={<PostList />} />
+          <Route path="/posts" element={<PostCategory />} />
+          <Route path="/posts/search" />
         </Routes>
       </Router>
-    </>
+    </RecoilRoot>
   );
 }
 
