94b2aa716695b18832002aab0597b0460c6e45a9
chaeeunj <official2930@naver.com>
2023-10-23 18:48:49 +0900

    :sparkles:Add: (chaeeun/feature/component/#49) 검색어를 저장하는 SearchKeyword Atom 생성

--- solumon-front/src/recoil/AllAtom.js
+++ solumon-front/src/recoil/AllAtom.js
@@ -1,25 +1,24 @@
 import { atom } from 'recoil';
 
-export const UserInterestTopic = atom({
-  key: 'UserInterestTopic',
-  default: {
-    member_id: 1,
-    interests: ['연애', '드라마/영화', '다이어트'],
-  },
+export const kakaoToken = atom({
+  key: 'kakaoToken',
+  default: '',
 });
-
-// export const kakaoToken = atom({
-//   key: 'kakaoToken',
-//   default: '',
-// });
 
 export const GeneralUserInfo = atom({
   key: 'GeneralUserInfo',
   default: [
     {
       accessToken: '',
-      firstLogIn: true,
+      firstLogIn: false,
       memberId: 0,
+      nickname: '',
+      interests: [],
     },
   ],
+});
+
+export const SearchKeyword = atom({
+  key: 'SearchKeyword',
+  default: '',
 });
