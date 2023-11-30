3ed3f9895484d256c5d347ad29957a2a322ed7c5
chaeeunj <official2930@naver.com>
2023-12-01 03:06:33 +0900

    :truck:Add: (chaeeun/feature/component/#69) firebase 파일 이름 변경 & firestore 사용을 위한 코드 추가

--- solumon-front/src/firebase-config.js
+++ solumon-front/src/firebase-config.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
@@ -14,6 +14,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
 const db = getFirestore(app);
 
-export { app, auth, db };
+export { app, auth, provider, db };
