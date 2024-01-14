061661636a21c0dba8bf230d26137cd8f7c9133b
chaeeunj <official2930@naver.com>
2024-01-14 18:11:23 +0900

    ✨:Feat: (chaeeun/feature/component/#69)  firestore에서 가져온 게시글 데이터로  제목&태그 검색결과를 필터링 하도록 구현

--- solumon-front/src/components/SearchBar.jsx
+++ solumon-front/src/components/SearchBar.jsx
@@ -1,78 +1,75 @@
 import { useState, useEffect } from 'react';
-import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { useRecoilState } from 'recoil';
 import { SearchKeyword } from '../recoil/AllAtom';
+import { db } from '../firebase-config';
+import { getDocs, collection, orderBy, query } from 'firebase/firestore';
 import styled, { ThemeProvider } from 'styled-components';
 import theme from '../style/theme';
 
 import PostCard from './PostCard';
 import { CiSearch } from 'react-icons/ci';
 
-const postSearchType = [
-  {
-    searchType: 'CONTENT',
-    title: '제목 및 본문',
-  },
-  {
-    searchType: 'TAG',
-    title: '태그',
-  },
-];
-
 function SearchBar() {
-  const userInfo = JSON.parse(window.localStorage.getItem('userInfo'));
-  const USER_TOKEN = userInfo.accessToken;
-
   const [keyword, setKeyword] = useRecoilState(SearchKeyword);
-  const [searchData, setSearchData] = useState([]);
+  const [tagSearchData, setTagSearchData] = useState([]);
+  const [titleSearchData, setTitleSearchData] = useState([]);
 
-  const fetchSearchData = async () => {
-    try {
-      const promises = postSearchType.map(async (type) => {
-        try {
-          const response = await axios.get(
-            `http://solumon.site:8080/posts/search?keyWord=${keyword}&postStatus=ONGOING&postOrder=LATEST&searchType=${type.searchType}&pageNum=1`,
-            {
-              headers: {
-                'X-AUTH-TOKEN': USER_TOKEN,
-                'Content-Type': 'application/json',
-              },
-              withCredentials: true,
-            },
-          );
-
-          if (response.status === 200) {
-            const jsonData = response.data;
-            setSearchData((prevData) => ({
-              ...prevData,
-              [type.searchType]: jsonData.content,
-            }));
-          } else {
-            console.log('검색 실패');
-          }
-        } catch (error) {
-          console.log(`Something Wrong: ${error.message}`);
-        }
+  const fetchData = async () => {
+    const querySnapshot = await getDocs(
+      query(collection(db, 'posts-write'), orderBy('created_at', 'desc')),
+    );
+    const dataList = [];
+    querySnapshot.forEach((doc) => {
+      dataList.push({
+        postId: doc.id,
+        ...doc.data(),
       });
+    });
 
-      await Promise.all(promises)
-        .then(() => {
-          console.log(searchData);
-        })
-        .catch((error) => {
-          console.log(`Error loading data: ${error}`);
-        });
-    } catch (error) {
-      console.log(`Something went wrong outside of the map: ${error}`);
+    return dataList;
+  };
+
+  const filterTagSearchData = async () => {
+    const data = await fetchData();
+
+    if (data) {
+      const filteredData = data.filter(
+        (item) =>
+          item.tags &&
+          Array.isArray(item.tags.hashTag) &&
+          item.tags.hashTag.includes(keyword),
+      );
+
+      setTagSearchData(filteredData);
+    }
+  };
+
+  const filterTitleSearchData = async () => {
+    const data = await fetchData();
+
+    if (data) {
+      const filteredData = data.filter((item) => item.title.includes(keyword));
+      setTitleSearchData(filteredData);
     }
   };
 
   const SearchClick = () => {};
 
   useEffect(() => {
-    fetchSearchData();
-  }, [keyword, postSearchType]);
+    filterTagSearchData();
+    filterTitleSearchData();
+  }, [keyword]);
+
+  const renderSearchResultSection = (title, searchType, postData) => (
+    <SearchResultSection>
+      <SectionTitle>{title} 검색결과</SectionTitle>
+      <AllPostsLink to={`/search/${searchType}/results/${keyword}`}>
+        전체보기 {'>'}
+      </AllPostsLink>
+      <PostCard postData={postData} postCount={5} />
+    </SearchResultSection>
+  );
 
   return (
     <>
@@ -85,20 +82,12 @@ function SearchBar() {
             placeholder="검색어를 입력하세요"
           ></SearchInput>
         </SearchContainer>
-        {keyword &&
-          postSearchType.map((item, idx) => (
-            <SearchResultSection key={idx}>
-              <SectionTitle>{item.title} 검색결과</SectionTitle>
-              <AllPostsLink to={`/search/${item.searchType}/results`}>
-                전체보기 {'>'}
-              </AllPostsLink>
-              <PostCard
-                postData={searchData}
-                postOrder={item.searchType}
-                postCount={5}
-              />
-            </SearchResultSection>
-          ))}
+        {keyword && (
+          <>
+            {renderSearchResultSection('태그', 'TAG', tagSearchData)}
+            {renderSearchResultSection('제목', 'CONTENT', titleSearchData)}
+          </>
+        )}
       </ThemeProvider>
     </>
   );
