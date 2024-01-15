91fbfda6421ad45332577aa66676559c08603c30
eunsoo-cho <127170596+eunsoo-cho@users.noreply.github.com>
2024-01-15 15:01:41 +0900

    :sparkles: [Feature/component/#15]Firebase 사용하여 SignupGeneral페이지 수정
    Feat:eunsoo-cho/featrue/component/#69

--- solumon-front/src/pages/sign-up/SignUpGeneral.jsx
+++ solumon-front/src/pages/sign-up/SignUpGeneral.jsx
@@ -1,105 +1,43 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
+import { auth } from '../../firebase-config';
+import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
 import styled, { ThemeProvider } from 'styled-components';
 import theme from '../../style/theme';
 import Button from '../../components/Button';
+import { addDoc, collection } from 'firebase/firestore';
+import { db } from '../../firebase-config';
 
 function SignUpGeneral() {
   const [userData, setUserData] = useState(null);
   const [nickname, setNickname] = useState('');
   const [email, setEmail] = useState('');
-  const [sendEmailAuthMsg, setSendEmailAuthMsg] = useState(false); // 이메일 전송 됐는지 여부
-  const [emailAuthCode, setEmailAuthCode] = useState('');
-  const [emailAuthCodeConfirm, setEmailAuthCodeConfirm] = useState('');
-  const [canUseEmail, setCanUseEmail] = useState('');
   const [password, setPassword] = useState('');
   const [checkPassword, setCheckPassword] = useState('');
-  const [canSignUp, setCanSignUp] = useState([
-    {
-      emailAuth_button_click: false, // 이메일 인증버튼 클릭했는지 확인
-      emailAuth_confirm_button_click: false, // 이메일 인증번호 확인했는지 확인
-    },
-  ]);
 
   const navigate = useNavigate();
 
-  // 이메일 인증 버튼 클릭
-  const handleEmailAuthButton = async (e) => {
+  // 파이어베이스를 사용한 회원가입
+  const handleSignUp = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.get(
-        `http://solumon.site:8080/user/send-email-auth?email=${email}`,
-        {
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        },
+      const result = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password,
       );
-
-      if (response.status === 200) {
-        console.log(response.data);
-        setEmailAuthCode(response.data.code); // 이메일 인증번호 저장
-        setSendEmailAuthMsg(true); // 이메일 전송 메세지 띄움
-        setCanSignUp({
-          ...canSignUp,
-          emailAuth_button_click: true,
+      if (result) {
+        await updateProfile(result.user, { displayName: nickname });
+        //사용자 uid와 닉네임 'users'라는 컬렉션에 파이어스토어에 저장
+        await addDoc(collection(db, 'users'), {
+          uid: result.user.uid,
+          nickName: nickname,
         });
-      } else {
-        console.error('이메일 정보 전송 X');
+        alert('회원가입이 완료되었습니다. 로그인 페이지로 이동합니다.');
+        navigate('/login');
       }
-    } catch (error) {
-      console.error('오류 발생: ' + error.message);
-    }
-  };
-
-  // 이메일 인증번호 입력 후 확인버튼 클릭
-  const handleEmailAuthConfirmButton = (e) => {
-    e.preventDefault();
-
-    // 인증번호 전송 후 받아온 response의 인증번호와 유저가 입력한 인증번호가 같은지 판단
-    emailAuthCode === emailAuthCodeConfirm
-      ? setCanUseEmail('사용 가능한 이메일 입니다.')
-      : setCanUseEmail('인증번호가 일치하지 않습니다.');
-
-    setCanSignUp({
-      ...canSignUp,
-      emailAuth_confirm_button_click: true,
-    });
-  };
-
-  // 회원가입 버튼 클릭
-  const handleSignUpButton = async (e) => {
-    e.preventDefault();
-    if (
-      // 이메일 인증 버튼과 인증번호 확인 버튼이 모두 클릭됐다면
-      canSignUp.emailAuth_button_click &&
-      canSignUp.emailAuth_confirm_button_click
-    ) {
-      try {
-        // 회원가입 요청 보내기
-        const response = await axios.post(
-          'http://solumon.site:8080/user/sign-up/general',
-          { nickname: nickname, email: email, password: password },
-          {
-            headers: {
-              'Content-Type': 'application/json',
-            },
-          },
-        );
-
-        if (response.status === 200) {
-          console.log(response.data);
-          alert('회원가입이 완료되었습니다.');
-          navigate('/login'); // 회원가입 완료 후 로그인 페이지로 이동
-        } else {
-          response.data.errorMessage && alert(response.data.errorMessage);
-        }
-      } catch (error) {
-        console.error('오류 발생: ' + error.message);
-      }
-    } else {
-      alert('이메일 인증 후에 회원가입이 가능합니다.');
+    } catch (err) {
+      console.log(err);
     }
   };
 
@@ -115,40 +53,14 @@ function SignUpGeneral() {
             onChange={(e) => setNickname(e.target.value)}
             required
           ></StyledInput>
-          <EmailAuthWrapper>
-            <EmailAuthInput
-              type="email"
-              placeholder="이메일 주소"
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            ></EmailAuthInput>
-            <Button
-              type="button"
-              name={'인증'}
-              onClick={handleEmailAuthButton}
-              fontSize={'14px'}
-              padding={'8px 17px'}
-            />
-          </EmailAuthWrapper>
-          {sendEmailAuthMsg && (
-            <CheckMessage>이메일로 인증번호가 전송되었습니다.</CheckMessage>
-          )}
-          <EmailAuthWrapper>
-            <EmailAuthInput
-              type="number"
-              placeholder="인증번호"
-              onChange={(e) => setEmailAuthCodeConfirm(e.target.value)}
-              required
-            ></EmailAuthInput>
-            <Button
-              type="button"
-              name={'확인'}
-              onClick={handleEmailAuthConfirmButton}
-              fontSize={'14px'}
-              padding={'8px 17px'}
-            />
-          </EmailAuthWrapper>
-          {canUseEmail && <CheckMessage>{canUseEmail}</CheckMessage>}
+
+          <EmailAuthInput
+            type="email"
+            placeholder="이메일 주소"
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          ></EmailAuthInput>
+
           <StyledInput
             type="password"
             placeholder="비밀번호 (8~20자)"
@@ -188,10 +100,10 @@ function SignUpGeneral() {
           )}
 
           <Button
-            type="submit"
+            type="button"
             value="sign-up-general"
             name={'회원가입'}
-            onClick={handleSignUpButton}
+            onClick={handleSignUp}
             fontSize={'16px'}
             padding={'10px 13px'}
           />
@@ -259,10 +171,6 @@ const InfoText = styled.p`
   margin: 10px 0;
   padding: 12px 15px;
   border-radius: 10px;
-`;
-
-const EmailAuthWrapper = styled.div`
-  display: flex;
 `;
 
 const EmailAuthInput = styled.input`
